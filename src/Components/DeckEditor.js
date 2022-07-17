d0e49d3999352e423e05164a4524cbbdfdf9ee71
Casey Geist <tonewardbound@gmail.com>
2022-07-17 17:19:47 -0400

    sending more callbacks as props

--- src/Components/DeckEditor.js
+++ src/Components/DeckEditor.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { decksApi } from '../DecksAPI';
 import '../App.css';
-import Deck from '../Deck.mjs';
+import { Deck } from '../Deck.mjs';
 import CardList from './CardList';
 
 export default class DeckEditor extends React.Component {
@@ -34,7 +34,8 @@ export default class DeckEditor extends React.Component {
         this.handleSearch = this.handleSearch.bind(this);
         this.addCard = this.addCard.bind(this);
         this.addFour = this.addFour.bind(this);
-        this.removeOne = this.removeOne.bind(this);
+        this.updateQuantity = this.updateQuantity.bind(this);
+        this.removeCard = this.removeCard.bind(this);
         this.removeAll = this.removeAll.bind(this);
         this.cancelEdit = this.cancelEdit.bind(this);
         this.getUpdatedColors = this.getUpdatedColors.bind(this);
@@ -53,6 +54,7 @@ export default class DeckEditor extends React.Component {
     handleCardNameChange(evt) {  this.setState({cardSearchText: evt.target.value});  }
 
     async handleSearch() {
+        if(!this.state.cardSearchText) return;
         const data = await decksApi.getCard(this.state.cardSearchText);
         if(data.cardName) this.setState({
             cardName: data.cardName, cardID: data.cardID, cardSrc: `/images/${data.cardID}.jpeg`
@@ -61,31 +63,29 @@ export default class DeckEditor extends React.Component {
     }
 
     addCard() {
-        if(!this.tempDeck || !this.state.cardName) {
-            // notify user that deck and card must be loaded first
-            return;
-        }
-        this.tempDeck.addCard(this.state.cardName, this.state.cardID);
-        this.setState({ cardCount: this.state.cardCount + 1});
-        console.log(this.state.cardName, "added to currentDeck");
+        if(!this.state.deckName || !this.state.cardName) return;
+        const wasAdded = this.tempDeck.addCard(this.state.cardName, this.state.cardID);
+        if(wasAdded) this.setState({ cardCount: this.state.cardCount + 1 });
     }
     addFour() {
         if(!this.state.deckName || !this.state.cardName) return;
-        this.tempDeck.addCard(this.state.cardName, this.state.cardID, 4);
-        this.setState({ cardCount: this.state.cardCount + 4});
-        console.log(this.state.cardName, "added 4x to currentDeck");
+        const wasAdded = this.tempDeck.addCard(this.state.cardName, this.state.cardID, 4);
+        if(wasAdded) this.setState({ cardCount: this.state.cardCount + 4 });
     }
-    removeOne() {
-        if(!this.state.deckName || !this.state.cardName) return;
-        this.tempDeck.removeCard(this.state.cardID);
-        if(this.state.cardCount > 0) this.setState({ cardCount: this.state.cardCount - 1});
-        console.log(this.state.cardName, "removed from currentDeck");
+    updateQuantity(id, qty) {
+        // if(!this.state.deckName) return;
+        const difference = Number(this.tempDeck.updateCardQty(id, qty));
+        
+        this.setState({ cardCount: this.state.cardCount + Number(difference)});
+    }
+    removeCard(id) {
+        const numRemoved = this.tempDeck.removeCard(id);
+        this.setState({ cardCount: this.state.cardCount - Number(numRemoved)});
     }
     removeAll() {
-        if(!this.state.deckName || !this.state.cardName) return;
+        if(!this.state.deckName) return;
         this.tempDeck.removeAllCards();
-        this.setState({ cardCount: 0});
-        console.log("all cards removed from currentDeck");
+        this.setState({ cardCount: 0 });
     }
 
     cancelEdit() {
@@ -111,8 +111,9 @@ export default class DeckEditor extends React.Component {
         if(!this.state.deckName) return;
         const deck = new Deck(this.state.deckName, this.state.deckID, this.getUpdatedColors());
         deck.cards = this.tempDeck.cards;
-        const updated = await decksApi.updateDeck(deck);
-        console.log("deck updated:", updated);
+        deck.sortByName();
+        await decksApi.updateDeck(deck);
+        console.log("deck updated");
         this.cancelEdit();
     }
 
@@ -125,8 +126,8 @@ export default class DeckEditor extends React.Component {
             this.setState({                                  
                 deckName: currentDeck.name,
                 deckID: currentDeck.id,
-                cards: this.tempDeck.cards,
-                cardCount: currentDeck.cards.length,
+                cards: this.tempDeck.cards,     // state.cards  POINTS TO  tempDeck.cards
+                cardCount: currentDeck.cards.reduce((sum, card) => sum + Number(card.qty), 0),
 
                 cbWhite: currentDeck.colors.includes("white"),
                 cbBlue: currentDeck.colors.includes("blue"),
@@ -137,6 +138,9 @@ export default class DeckEditor extends React.Component {
             });
             
         }
+        else if(prevprops.currentDeck && !currentDeck) {                     // else vice versa
+            this.cancelEdit();
+        }
     }
 
     render() {
@@ -144,20 +148,38 @@ export default class DeckEditor extends React.Component {
             <div className='flex-row'>
                 <div id="editor-panel" className='cont'>
                     <h2>{this.props.currentDeck ? `Editing "${this.props.currentDeck.name}"` : "Editor"}</h2>
-                    <div className='flex-start'>
-                        <label>ID: &nbsp;</label>
-                        <input className='ro-input' type="text" value={this.props.currentDeck ? this.props.currentDeck.id : ''} readOnly/> &nbsp;&nbsp;
-                        <label>Name: &nbsp;</label>
-                        <input  type="text" value={this.state.deckName} onChange={this.handleDeckNameChange}/> 
+                    <div className='flex-start' style={{marginLeft: "20px"}}>
+                        <label>ID: &nbsp;
+                            <input className='sm-input' type="text" value={this.props.currentDeck ? this.props.currentDeck.id : ''} readOnly/> &nbsp;&nbsp;
+                        </label> &nbsp;
+                        <label>Name: &nbsp;
+                            <input style={{width: "180px"}} type="text" value={this.state.deckName} onChange={this.handleDeckNameChange}/> 
+                        </label>
                     </div>
                     <div style={{marginLeft: "20px"}}>
                         <br></br>
-                        <input type="checkbox" value="white" checked={this.state.cbWhite} onChange={this.handleCheckboxWhite}/> <label>White</label>
-                        <input type="checkbox" value="blue" checked={this.state.cbBlue} onChange={this.handleCheckboxBlue}/> <label>Blue</label>
-                        <input type="checkbox" value="black" checked={this.state.cbBlack} onChange={this.handleCheckboxBlack}/> <label>Black</label><br></br>
-                        <input type="checkbox" value="red" checked={this.state.cbRed} onChange={this.handleCheckboxRed}/> <label>Red</label>
-                        <input type="checkbox" value="green" checked={this.state.cbGreen} onChange={this.handleCheckboxGreen}/> <label>Green</label>
-                        <input type="checkbox" value="colorless" checked={this.state.cbColorless} onChange={this.handleCheckboxColorless}/> <label>Colorless</label>
+                        <div className='cbl-box'>
+                            <label >
+                                <input type="checkbox" value="white" checked={this.state.cbWhite} onChange={this.handleCheckboxWhite}/>
+                            &nbsp; White</label>
+                            <label >
+                                <input type="checkbox" value="blue" checked={this.state.cbBlue} onChange={this.handleCheckboxBlue}/> 
+                            &nbsp; Blue</label>
+                            <label >
+                                <input type="checkbox" value="black" checked={this.state.cbBlack} onChange={this.handleCheckboxBlack}/> 
+                            &nbsp; Black</label>
+                        </div>
+                        <div className='cbl-box'>
+                            <label >
+                                <input type="checkbox" value="red" checked={this.state.cbRed} onChange={this.handleCheckboxRed}/> 
+                            &nbsp; Red</label>
+                            <label >
+                                <input type="checkbox" value="green" checked={this.state.cbGreen} onChange={this.handleCheckboxGreen}/> 
+                            &nbsp; Green</label>
+                            <label >
+                                <input type="checkbox" value="colorless" checked={this.state.cbColorless} onChange={this.handleCheckboxColorless}/> 
+                            &nbsp; Colorless</label>
+                        </div>    
                     </div>
                     <br></br>
                     <div id="card-n-ctrls">
@@ -168,16 +190,17 @@ export default class DeckEditor extends React.Component {
                             <div>
                                 <button onClick={this.addCard} className='btn btn-green'>+1</button><br></br><br></br>
                                 <button onClick={this.addFour} className='btn btn-green'>+4</button><br></br><br></br>
-                                <button onClick={this.removeOne} className='btn btn-red'>-1</button><br></br><br></br>
+                                {/* <button onClick={this.removeOne} className='btn btn-red'>-1</button><br></br><br></br> */}
                                 <button onClick={this.removeAll} className='btn btn-red'>-all</button>
                             </div>
                             <div>
-                                <input className='ro-input' type="number" value={this.state.cardCount} readOnly/>
+                                <label>Total:</label><br></br>
+                                <input className='sm-input' type="number" value={this.state.cardCount} readOnly/>
                             </div>
                         </div>    
                     </div><br></br>
-                    <div className='flex-start'>
-                        <input className='w-200' type="text" value={this.state.cardSearchText} onChange={this.handleCardNameChange} /> &nbsp;
+                    <div className='flex-start' style={{marginLeft: "20px"}}>
+                        <input style={{width: "180px"}} type="text" value={this.state.cardSearchText} onChange={this.handleCardNameChange} /> &nbsp;
                         <button onClick={this.handleSearch} className='btn btn-blue'>&#128269;</button>
                     </div>
                     <br></br>
@@ -189,13 +212,11 @@ export default class DeckEditor extends React.Component {
                     </div>
                 </div>
                 <div id="card-list" className='cont'>
-                    {this.state.cards ? <CardList cards={this.state.cards.filter((card, index, array) => {
-                        if(index+1 < array.length) return (card.id !== array[index+1].id);
-                        else return true; } )} /> 
+                    {this.state.cards ? <CardList cards={this.state.cards} 
+                                                  updateQty={this.updateQuantity} delete={this.removeCard}/> 
                         : <p>No cards to display</p> }
                 </div>
             </div>
         );
     }
 }
-    
