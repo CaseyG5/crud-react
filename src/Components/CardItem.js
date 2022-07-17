2fcd59c454a23dba26c647f008925f4be96831a2
Casey Geist <tonewardbound@gmail.com>
2022-07-17 17:25:54 -0400

    added update & delete buttons

--- src/Components/CardItem.js
+++ src/Components/CardItem.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import '../App.css';
 
 export default function CardItem(props) {
+    const [quantity, setQuantity] = useState( Number(props.quantity) );
+
+    useEffect( () => {
+        setQuantity( Number(props.quantity) );
+    }, [props.quantity]);
+
+    const handleUpdateQty = (id, qty) => {
+        props.updateQty(id, qty);
+    };
 
     return(
-        <div>{props.qty} &nbsp; {props.name}</div>
+        <div className='flex-betw' style={{marginBottom: "1px"}}>
+            <div>
+                <input className='sm-input' type="number" min="0" max="4"
+                        value={quantity} onChange={(evt) => setQuantity(Number(evt.target.value)) } />
+                {"  "}{props.name}
+            </div>
+            <div className='btn-duo-sm'>
+                <button onClick={(evt) => {
+                            evt.preventDefault();
+                            handleUpdateQty(props.id, quantity);
+                        }} className='btn-sm btn-blue'>&#10004;</button>
+                {"  "}<button onClick={(evt) => {
+                            evt.preventDefault();
+                            props.delete(props.id);
+                        }} className='btn-sm btn-red'>&#10008;</button>
+            </div>
+        </div>
     );
 }
